83c8d7a6ab8e743fec23cb2663b9345ab0951042
SantLop01 <gabsant2001@gmail.com>
2024-03-18 19:15:01 -0300

    [FIX] Changed some styles and links added

--- js/data.js
+++ js/data.js
@@ -1,13 +1,19 @@
 export const data = {
     es: {
         title: 'En construcción',
-        description: 'Estamos en plena construcción de nuestra plataforma. Pronto estaremos listos para ofrecerte única, para que puedas cambiar de vida regresando a tus raíces.',
+        description: `Estamos en plena construcción de nuestra plataforma. 
+        Pronto estaremos listos para ofrecerte una experiencia única, 
+        para que puedas cambiar de vida regresando a tus raíces.`,
         description_contact: '¿Deseas ponerte en contacto con nosotros? Dejanos tu email aquí',
         send_text: 'Enviar'
     },
     it: {
         title: 'Lavori in corso',
-        description: 'Siamo una piattaforma che utilizza tecnologia all’avanguardia per facilitare il ritorno e l’integrazione degli italiani residenti all’estero, sostenendo le aziende nella ricerca di talenti e contribuendo così allo sviluppo sociale ed economico dell’Italia.',
+        description: `Siamo una piattaforma che utilizza tecnologia all’avanguardia per
+        facilitare il ritorno e l’integrazione degli italiani residenti all’estero,
+        sostenendo le aziende nella ricerca di talenti e contribuendo così 
+        allo sviluppo sociale ed economico dell’Italia.
+        `,
         description_contact: 'Desideri metterti in contatto con noi? Lasciaci la tua email qui',
         send_text: 'Invia'
     }
--- js/main.js
+++ js/main.js
@@ -13,20 +13,30 @@ const description = document.querySelector('.info__description');
 const description_contact = document.querySelector('.info__description.contact');
 const send_text = document.querySelector('.form__right button');
 
-console.log(italian.className);
+// Botones de direcciónes
+const instagram = document.getElementById('ig');
+const linkedin = document.getElementById('in');
+const whatsapp = document.getElementById('wp');
 
 /* Functions */
 spanish.addEventListener('click', () => {
+    // Guardando el idioma
     localStorage.setItem('lang', 'es');
     if (italian.className === 'idiom__btn active') {
         italian.classList.remove('active');
     };
     spanish.classList.add('active');
 
+    // Cambiando los textos
     title.textContent = es.title;
-    description.textContent = es.description;
+    description.innerHTML = es.description;
     description_contact.textContent = es.description_contact;
     send_text.textContent = es.send_text;
+
+    // Redireccionando a redes
+    instagram.href = 'https://www.instagram.com/back2italy.la/';
+    linkedin.href = 'https://www.linkedin.com/company/back2italy';
+    whatsapp.href = 'https://wa.me/3904321510622';
 });
 
 italian.addEventListener('click', () => {
@@ -37,7 +47,7 @@ italian.addEventListener('click', () => {
     italian.classList.add('active');
 
     title.textContent = it.title;
-    description.textContent = it.description;
+    description.innerHTML = it.description;
     description_contact.textContent = it.description_contact;
     send_text.textContent = it.send_text;
 });
